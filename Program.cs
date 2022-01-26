35178825585a0c93570327e997c562cf4652b168
Daniel Gyoerffy <49782833+Dansoftowner@users.noreply.github.com>
2022-01-26 19:16:23 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LifeOrDeath11A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
